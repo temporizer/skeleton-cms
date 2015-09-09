3fd2925f38a5d3a5906cc76f900b9f89a8430141
Nicholas Eli <nick@nicholasbydesign.com>
2015-09-09 00:52:22 -0700

    add 1 to 1 ratio for screen size in admin php and login php

--- admin.php
+++ admin.php
@@ -7,8 +7,9 @@
 		<!DOCTYPE html>
 		<html lang="en">
 		<head>
+			<meta name="viewport" content="width=device-width, initial-scale=1">
 			<meta charset="UTF-8">
-			<title>Admin</title>
+			<title>Skeleton CMS Admin</title>
 			<link rel="stylesheet" href="css/normalize.css">
 			<link rel="stylesheet" href="css/skeleton.css">
 			<link rel="stylesheet" href="css/admin.css">
--- login.php
+++ login.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta charset="UTF-8">
-	<title>Login</title>
+	<title>Skeleton CMS Login</title>
 	<link rel="stylesheet" href="css/normalize.css">
 	<link rel="stylesheet" href="css/skeleton.css">
 	<link rel="stylesheet" href="css/admin.css">
